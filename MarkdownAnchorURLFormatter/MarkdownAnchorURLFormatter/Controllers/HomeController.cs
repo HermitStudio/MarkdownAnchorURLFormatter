eb901ea161468dea63d6ab932daab45a57cbf12d
Corrie Allan <corrie@CPH-LAP-CORRIE.local>
2018-07-23 16:01:35 +0200

    Adding inital version of the formatting tool

---
+++ MarkdownAnchorURLFormatter/MarkdownAnchorURLFormatter/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using System.Collections.Specialized;
+using System.Text.RegularExpressions;
+
+namespace MarkdownAnchorURLFormatter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+                      
+            return View();
+        }
+        
+        public ActionResult Result()
+        {
+            ViewBag.Message = "Your result page.";
+
+            return View();
+        }
+    }
+}
